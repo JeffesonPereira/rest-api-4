e4a9f160b2132a6585ed5268632b240e686a5231
Jeffeson Gonçalves <jeffesongoncalves@hotmail.com>
2022-03-22 10:27:25 -0300

    dotenv bd

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,12 +1,24 @@
 require("dotenv").config();
 const pgp = require('pg-promise')();
 
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_PORT = process.env.DB_PORT;
+const DN_NAME = process.env.DN_NAME;
+
 const db = pgp({
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DN_NAME
+	user: DB_USER,
+	password: DB_PASSWORD,
+	host: DB_HOST,
+	port: DB_PORT,
+	database: DN_NAME
 });
 
 module.exports = db;
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DN_NAME);
